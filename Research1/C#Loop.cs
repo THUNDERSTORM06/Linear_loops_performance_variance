db8803d5bef8a4a7a09410934971eea472dfe258
Raul Anca <44055338+THUNDERSTORM06@users.noreply.github.com>
2024-02-16 16:40:30 +0200

    Add files via upload
    added another variable instead of using i=i

---
+++ Research1/C#Loop.cs
@@ -0,0 +1,12 @@
+static void Main(string[] args)
+{
+    for (int i = 0; i < 1000; i++)
+    {
+       if(i>1000)
+            {
+                i=i+1;
+            }
+ 
+    }
+}
+
---
+++ Research1/RubyLoop.rb
@@ -0,0 +1,5 @@
+(1..1000).each do |i|
+    if i>1000
+    	i=i+1
+    end
+end
