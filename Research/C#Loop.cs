cd5a7a7d1ab5108f4c3fb61edbc80b6d54102a4e
Raul Anca <44055338+THUNDERSTORM06@users.noreply.github.com>
2024-02-16 15:45:56 +0200

    Add files via upload
    The first type of for loops,that go through 1000 elements,and says that the element is attributed  it's own value.

---
+++ Research/C#Loop.cs
@@ -0,0 +1,8 @@
+static void Main(string[] args)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        i=i; 
+    }
+}
+
---
+++ Research/JavaLoop.java
@@ -0,0 +1,7 @@
+public class JavaLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1000; i++) {
+            i=i;
+        }
+    }
+}
